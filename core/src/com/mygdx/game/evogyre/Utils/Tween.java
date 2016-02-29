cf259573794a55f18ab3dbf50ab811bf5b823b70
Jay W Johnson <jwj@boun.cr>
2016-02-29 22:14:31 +0800

    Tween class refactored and QuadInOut added

--- core/src/com/mygdx/game/evogyre/Utils/Tween.java
+++ core/src/com/mygdx/game/evogyre/Utils/Tween.java
@@ -8,12 +8,12 @@ package com.mygdx.game.evogyre.Utils;
 public class Tween {
     private static final String TAG = Tween.class.getName();
 
-    public static class SineOut {
-        private float timePassed;
-        private float startValue;
-        private float endValue;
-        private float changeValue;
-        private float duration;
+    public static class TweenBase {
+        public float timePassed;
+        public float startValue;
+        public float endValue;
+        public float changeValue;
+        public float duration;
 
         /**
          * Constructor for SineOut tween equation.
@@ -21,12 +21,29 @@ public class Tween {
          * @param c Finish value
          * @param d Duration of tween
          */
-        public SineOut(float b, float c, float d) {
+        public TweenBase(float b, float c, float d) {
             timePassed = 0f;
             startValue = b;
             endValue = c;
             changeValue = c-b;
             duration = d;
+        }
+
+        public boolean isDone() {
+            if (timePassed >= duration) return true;
+            return false;
+        }
+    }
+
+    public static class SineOut extends TweenBase {
+        /**
+         * Constructor for SineOut tween equation.
+         * @param b Start value
+         * @param c Finish value
+         * @param d Duration of tween
+         */
+        public SineOut(float b, float c, float d) {
+            super(b, c, d);
         }
 
         /**
@@ -38,6 +55,32 @@ public class Tween {
             timePassed += delta/10f;
             if (timePassed > duration) return endValue;
             return changeValue * (float)Math.sin(timePassed / duration * (Math.PI / 2)) + startValue;
+        }
+    }
+
+    public static class QuadInOut extends TweenBase {
+        /**
+         * Constructor for SineOut tween equation.
+         * @param b Start value
+         * @param c Finish value
+         * @param d Duration of tween
+         */
+        public QuadInOut(float b, float c, float d) {
+            super(b, c, d);
+        }
+
+        /**
+         * Returns the next updated value.
+         * @param delta Time passed since last call.
+         * @return
+         */
+        public float next(float delta) {
+            timePassed += delta;
+            if (timePassed > duration) return endValue;
+            float t = timePassed / (duration / 2);
+            if (t < 1) return changeValue / 2 * t * t + startValue;
+            t--;
+            return -changeValue/2 * (t * (t - 2) - 1) + startValue;
         }
     }
 }
